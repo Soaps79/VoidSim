a4e31adb221d68027d7af6e084d1a8c1a8ea2b2b
Daniel Sopel <dsopel@hotmail.com>
2017-03-10 04:31:48 +0000

    Stubbed out Products
    git-tfs-id: [https://tacticsiege.visualstudio.com/DefaultCollection]$/VoidSim/develop;C362

---
+++ src/VoidSim.Console/VoidSim.Console/Products/Product.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace VoidSim.Console.Products
+{
+    // Example Recipe: 1 Iron + 3 Basalt in a SmallFactory produce 2 BuildingMaterial
+    
+    // For sorting? Feels like it could be useful in many instances
+    public enum ProductCategory
+    {
+        Raw, Refined, Luxury
+    }
+
+    public class Ingredient
+    {
+        public string ProductName;
+        public int Quantity;
+    }
+
+    public class Recipe
+    {
+        public string ProductName;
+        public string ProductionContainerName;
+        public List<Ingredient> Ingredients;
+    }
+
+    public class Product
+    {
+        public ProductCategory Category;
+        public string Name;
+
+        // Value? If common currency (credits?) is a thing
+        // Quality?
+
+    }
+}
---
+++ src/VoidSim.Console/VoidSim.Console/Products/ProductTradingHub.cs
@@ -0,0 +1,41 @@
+﻿namespace VoidSim.Console.Products
+{
+    // This object is essentially a service to the rest of the game whose role is to connect consumers with providers. 
+    // The goods do not actually pass through here, Providers simply agree to send a specified amount to the Consumer. 
+    // If this amount does not fully meet the requested amount, that amount will be lessened by the contracted amount, 
+    // and the Hub will continue to try and find a Provider for the remainder.
+    public class ProductTradingHub
+    {
+        
+    }
+
+    // An actor in the trading system
+    // This object should eventually have information about 
+    // location, diplomatic faction, whatever the hub can use to decide if its a fit
+    // OR, does the hub ask the trader if he will willingly trade with the other?
+    public class ProductTrader
+    {
+        public string Name;
+    }
+
+    // One trade consideration could be Distance. Distance can work such that 
+    // if the distance is too far, the traders can buy fuel along the way (from us!)
+    // Good/easy early game income, maybe a small production chain to produce it
+    // Great material for a tutorial (you explored a planet nearby that has X! 
+    // You are already producing Y in your base, build a factory to combine them to make fuel.)
+
+
+    // ProductTraders will submit these requests to the hub when they either 
+    // need goods (Consume), or they have excess good they want to trade (Provide)
+    public enum ProductTransferAction
+    {
+        Provide, Consume
+    }
+
+    public class ProductRequest
+    {
+        public ProductTransferAction Action;
+        public string ProductName;
+        public int Amount;
+    }
+}
