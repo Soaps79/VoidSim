c0286d6db2825653fc3ea096e9c51c04b3cf9c45
Ryan Little <TacticSiege@gmail.com>
2017-03-10 02:24:31 +0000

    Adding initial grid
    git-tfs-id: [https://tacticsiege.visualstudio.com/DefaultCollection]$/VoidSim/develop;C359

---
+++ src/VoidSim/Assets/Scripts/GridController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using QGame;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GridController : QScript
+    {
+        private Grid _grid;
+        public int Width = 50;
+        public int Height = 50;
+
+        public Sprite SpaceTile;
+        public Sprite FloorTile;
+        public Sprite ScaffoldTile;
+        
+
+        void Start()
+        {
+            // Create grid model
+            _grid = new Grid(name, Width, Height);
+
+            // create game object for each tile
+            for (var i = 0; i < _grid.Width; i++)
+            {
+                for (var j = 0; j < _grid.Height; j++)
+                {
+                    var tileData = _grid.GetTileAt(i, j);
+                    var tileGo = new GameObject(tileData.Name);
+                    tileGo.transform.position = new Vector3(i, j, 0);
+                    tileGo.transform.parent = transform;
+
+                    var spriteRenderer = tileGo.AddComponent<SpriteRenderer>();
+                    spriteRenderer.sprite = AssignTileSprite(tileData.Type);
+                }
+            }
+        }
+
+        private Sprite AssignTileSprite(TileType tileType)
+        {
+            switch (tileType)
+            {
+                case TileType.Space:
+                    return SpaceTile;
+                case TileType.Scaffolding:
+                    return ScaffoldTile;
+                case TileType.Floor:
+                    return FloorTile;
+                default:
+                    throw new ArgumentOutOfRangeException("tileType", tileType, null);
+            }
+        }
+
+        public void OnTileTypeChanged(Tile tileData, GameObject tileGo)
+        {
+            tileGo.GetComponent<SpriteRenderer>().sprite = AssignTileSprite(tileData.Type);
+        }
+    }
+}
---
+++ src/VoidSim/Assets/Scripts/Tile.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public enum TileType { Space, Scaffolding, Floor }
+
+    public class Tile
+    {
+        
+
+        private readonly Grid _owner;
+
+        private readonly string _name;
+        private readonly int _x;
+        private readonly int _y;
+        private TileType _tileType = TileType.Space;
+        
+
+
+        public int X { get { return _x; } }
+        public int Y { get { return _y; } }
+        public string Name { get { return _name; } }
+        public TileType Type { get { return _tileType; } }
+
+        public Tile(Grid owner, int x, int y)
+        {
+            _owner = owner;
+            _x = x;
+            _y = y;
+            _name = string.Format("{0}-Tile:({1}, {2})", _owner.Name, _x, _y);
+        }
+
+        public bool CanBuild(TileType type)
+        {
+            if (type == TileType.Space)
+            {
+                return true;
+            }
+
+            if (type == TileType.Scaffolding)
+            {
+                return _tileType == TileType.Space
+                    || _tileType == TileType.Floor;
+            }
+
+            if (type == TileType.Floor)
+            {
+                return _tileType == TileType.Scaffolding;
+            }
+
+            Debug.LogError("CanBuild - Unknown TileType");
+            return false;
+        }
+    }
+}
