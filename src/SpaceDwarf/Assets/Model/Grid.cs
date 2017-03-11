7d9857d31ae8d78ab29a3faa779b515ce2ddabda
Ryan Little <TacticSiege@gmail.com>
2017-03-11 16:59:44 +0000

    Adding SpaceDwarf with inital copied grid
    git-tfs-id: [https://tacticsiege.visualstudio.com/DefaultCollection]$/VoidSim/develop;C370

---
+++ src/SpaceDwarf/Assets/Model/Grid.cs
@@ -0,0 +1,41 @@
+﻿namespace Assets.Model
+{
+    public class Grid
+    {
+        private readonly string _name;
+        private readonly int _width;
+        private readonly int _height;
+        private readonly Tile[,] _tiles;
+
+        public string Name { get { return _name; } }
+        public int Width { get { return _width; } }
+        public int Height { get { return _height; } }
+
+        public Grid(string name, int width = 50, int height = 50)
+        {
+            _name = name;
+            _width = width;
+            _height = height;
+
+            _tiles = InitializeTiles();
+        }
+
+        private Tile[,] InitializeTiles()
+        {
+            var tiles = new Tile[_width, _height];
+            for (var i = 0; i < _width; i++)
+            {
+                for (var j = 0; j < _height; j++)
+                {
+                    tiles[i, j] = new Tile(this, i, j);
+                }
+            }
+            return tiles;
+        }
+
+        public Tile GetTileAt(int x, int y)
+        {
+            return _tiles[x, y];
+        }
+    }
+}
---
+++ src/SpaceDwarf/Assets/Model/Tile.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Model
+{
+    public enum TileType { Space = 0, Scaffolding = 1, Floor = 2 }
+
+    public class Tile
+    {
+        private readonly Grid _owner;
+
+        private readonly string _name;
+        private readonly int _x;
+        private readonly int _y;
+        private TileType _tileType = TileType.Space;
+
+        private Action<Tile> _onTileTypeChanged;
+
+        public int X { get { return _x; } }
+        public int Y { get { return _y; } }
+        public string Name { get { return _name; } }
+
+        public TileType Type
+        {
+            get { return _tileType; }
+            set
+            {
+                if (_tileType != value)
+                {
+                    _tileType = value;
+                    if (_onTileTypeChanged != null)
+                        _onTileTypeChanged(this);
+                }
+            }
+        }
+
+        public Tile(Grid owner, int x, int y)
+        {
+            _owner = owner;
+            _x = x;
+            _y = y;
+            _name = string.Format("{0}-Tile:({1}, {2})", _owner.Name, _x, _y);
+        }
+
+        public bool CanBuild(TileType type)
+        {
+            if (type == TileType.Space)
+            {
+                return true;
+            }
+
+            if (type == TileType.Scaffolding)
+            {
+                return _tileType == TileType.Space
+                    || _tileType == TileType.Floor;
+            }
+
+            if (type == TileType.Floor)
+            {
+                return _tileType == TileType.Scaffolding;
+            }
+
+            Debug.LogError("CanBuild - Unknown TileType");
+            return false;
+        }
+
+        public void RegisterOnTileTypeChangedCallback(Action<Tile> callback)
+        {
+            _onTileTypeChanged += callback;
+        }
+    }
+}
