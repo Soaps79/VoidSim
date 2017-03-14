b63b961fb91ba2acdbd848b371b8e2dc14f8befa
Ryan Little <TacticSiege@gmail.com>
2017-03-14 19:12:47 +0000

    Example editor wizard.  Add another string to invocation to make one with 2 buttons.
    git-tfs-id: [https://tacticsiege.visualstudio.com/DefaultCollection]$/VoidSim/develop;C385

---
+++ src/SpaceDwarf/Assets/Editor/BasicPanelWizard.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    public class BasicPanelWizard : ScriptableWizard
+    {
+        public string PanelLabel = "Default Label";
+        public int Width = 420;
+        public int Height = 640;
+
+        public bool IsDraggable = true;
+        public bool IsResizable = false;
+
+        public GameObject InnerPanelContent = null;
+
+        [MenuItem("TacticLib/GUI/Create Basic Panel")]
+        static void CreateWizard()
+        {
+            DisplayWizard<BasicPanelWizard>("Create Basic Panel...", "Create");
+        }
+
+        void OnWizardCreate()
+        {
+            // create the panel game object
+
+            // add it to scene, or serialize it directly, whatever
+        }
+
+        void OnWizardUpdate()
+        {
+            helpString = "Enter panel properties";
+        }
+    }
+}
