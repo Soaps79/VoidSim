b574748b29de3ce9993982755d4fa4a96ae97825
Ryan Little <TacticSiege@gmail.com>
2017-03-14 18:47:03 +0000

    Example asset post processor
    git-tfs-id: [https://tacticsiege.visualstudio.com/DefaultCollection]$/VoidSim/develop;C384

---
+++ src/SpaceDwarf/Assets/Editor/SpriteProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    public class SpriteProcessor : AssetPostprocessor
+    {
+        //todo: configurable
+        private const string SpritePathSearchToken = "sprites";
+        private const int SpritePixelsPerUnit = 32;
+        void OnPostprocessTexture(Texture2D texture)
+        {
+            var path = assetPath.ToLowerInvariant();
+            var isInSpriteDir = path.Contains(SpritePathSearchToken);
+
+            if (!isInSpriteDir)
+                return;
+            
+            //todo: check for predefined sprite slicing file
+
+            var importer = assetImporter as TextureImporter;
+            if (importer == null)
+            {
+                throw new InvalidCastException("Unable to cast asset importer to TextureImporter");
+            }
+
+            Debug.Log("Imported Sprite");
+        }
+    }
+}
