41e2254aa9a2e55cb2734a2bff7e02646c778c65
Daniel Sopel <dsopel@hotmail.com>
2017-03-19 04:45:57 +0000

    Known Recipes are loaded into a build UI
    git-tfs-id: [https://tacticsiege.visualstudio.com/DefaultCollection]$/VoidSim/develop;C414

--- src/VoidSim/Assets/Scripts/Station.cs
+++ src/VoidSim/Assets/Scripts/Station.cs
@@ -5,82 +5,108 @@ using Assets.Scripts;
 using Assets.Scripts.WorldMaterials;
 using QGame;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 public enum LayerType
 {
-	Top, Middle, Bottom
+    Top, Middle, Bottom
 }
 
 [Serializable]
 public class StationLayer
 {
-	public LayerType Type;
-	public int BuildingGridCapacity;
+    public LayerType Type;
+    public int BuildingGridCapacity;
 }
 
 public class Station : QScript
 {
-	[Inject]
-	private WorldClock _worldClock;
-	[Inject]
-	private ProductLookup _productLookup;
+    [Inject]
+    private WorldClock _worldClock;
+    [Inject]
+    private ProductLookup _productLookup;
 
-	[SerializeField]
-	private StationLayer[] _initialLayers;
-	[SerializeField]
-	private GameObject _craftingPrefab;
+    [SerializeField]
+    private Image _craftingViewContext;
+    [SerializeField]
+    private Button _buttonPrefab;
 
-	private readonly Dictionary<LayerType, StationLayer> _layers =  new Dictionary<LayerType, StationLayer>();
-	private CraftingContainer _crafter;
+    [SerializeField]
+    private StationLayer[] _initialLayers;
+    [SerializeField]
+    private GameObject _craftingPrefab;
 
-	void Awake()
-	{
-		if (_initialLayers != null)
-		{
-			foreach (var initialLayer in _initialLayers)
-			{
-				_layers.Add(initialLayer.Type, initialLayer);
-			}
-		}
-	}
+    private readonly Dictionary<LayerType, StationLayer> _layers =  new Dictionary<LayerType, StationLayer>();
+    private CraftingContainer _crafter;
 
-	// Use this for initialization
-	void Start ()
-	{
-		CreateCraftingContainer();
-	}
+    void Awake()
+    {
+        if (_initialLayers != null)
+        {
+            foreach (var initialLayer in _initialLayers)
+            {
+                _layers.Add(initialLayer.Type, initialLayer);
+            }
+        }
+    }
 
-	private void CreateCraftingContainer()
-	{
-		// testing container basic functionality
-		var crafterGo = GameObject.Instantiate(_craftingPrefab);
-		crafterGo.transform.SetParent(transform);
-		var crafter = crafterGo.GetOrAddComponent<CraftingContainer>();
-		crafter.Info = _productLookup.GetContainers().FirstOrDefault();
-		// Move this to DI or Locator
-		crafter.WorldClock = _worldClock;
-		crafter.OnCraftingComplete += OnCraftingComplete;
-		_crafter = crafter;
+    // Use this for initialization
+    void Start ()
+    {
+        CreateCraftingContainer();
+        BindToUI();
+    }
 
-		Locator.ValueDisplay.Add("Crafting Queue", () => _crafter.CurrentQueueCount);
-		Locator.ValueDisplay.Add("Current Craft", () => _crafter.CurrentCraftRemainingAsZeroToOne);
-	}
+    private void CreateCraftingContainer()
+    {
+        // testing container basic functionality
+        var crafterGo = GameObject.Instantiate(_craftingPrefab);
+        crafterGo.transform.SetParent(transform);
+        var crafter = crafterGo.GetOrAddComponent<CraftingContainer>();
+        crafter.Info = _productLookup.GetContainers().FirstOrDefault();
+        // Move this to DI or Locator
+        crafter.WorldClock = _worldClock;
+        crafter.OnCraftingComplete += OnCraftingComplete;
+        _crafter = crafter;
 
-	private void OnCraftingComplete(Recipe recipe)
-	{
-		Debug.Log(string.Format("{0}: Craft Complete", recipe.ResultProduct));
-	}
+        Locator.ValueDisplay.Add("Crafting Queue", () => _crafter.CurrentQueueCount);
+        Locator.ValueDisplay.Add("Current Craft", () => _crafter.CurrentCraftRemainingAsZeroToOne);
+    }
 
-	public void CraftSomething()
-	{
-		_crafter.QueueCrafting(_productLookup.GetRecipes().FirstOrDefault());
-	}
+    private void BindToUI()
+    {
+        if(_craftingViewContext == null || _buttonPrefab == null)
+            throw new UnityException("Crafting UI ref missing");
 
-	public StationLayer GetLayer(LayerType type)
-	{
-		StationLayer toReturn = null;
-		_layers.TryGetValue(type, out toReturn);
-		return toReturn;
-	}
+        foreach (var recipe in _productLookup.GetRecipes())
+        {
+            var button = GameObject.Instantiate(_buttonPrefab).GetComponent<Button>();
+            var text = button.GetComponentInChildren<Text>();
+            text.text = recipe.Ingredients.Aggregate(string.Format("{0}\t\t", recipe.ResultProduct), (content, ing)
+                => content + string.Format("{0} {1} ", ing.Quantity, ing.ProductName));
+            button.transform.parent = _craftingViewContext.transform;
+            button.onClick.AddListener( () =>
+            {
+                _crafter.QueueCrafting(recipe);
+            });
+        }
+    }
+
+    private void OnCraftingComplete(Recipe recipe)
+    {
+        Debug.Log(string.Format("{0}: Craft Complete", recipe.ResultProduct));
+    }
+
+    public void CraftSomething()
+    {
+        _crafter.QueueCrafting(_productLookup.GetRecipes().FirstOrDefault());
+    }
+
+    public StationLayer GetLayer(LayerType type)
+    {
+        StationLayer toReturn = null;
+        _layers.TryGetValue(type, out toReturn);
+        return toReturn;
+    }
 }
