776d9f3af7a3ab0cdaa6c765789b01271262c2c2
Ryan Little <TacticSiege@gmail.com>
2017-03-21 04:45:49 +0000

    Global States with Pause, added Logger execution for debugging
    git-tfs-id: [https://tacticsiege.visualstudio.com/DefaultCollection]$/VoidSim/develop;C425

---
+++ src/SpaceDwarf/Assets/Model/GameStates/Executions/PauseControllers.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Controllers;
+using QGame;
+using UnityEngine;
+
+namespace Assets.Model.GameStates.Executions
+{
+    [CreateAssetMenu(menuName = "GameStates/Executions/Pause Controllers")]
+    public class PauseControllers : Execution
+    {
+        public override void Execute(StateMachine machine, float timeDelta)
+        {
+            TerrainController.Instance.enabled = false;
+            PlayerController.Instance.enabled = false;
+            CameraController.Instance.enabled = false;
+        }
+    }
+}
---
+++ src/SpaceDwarf/Assets/Model/GameStates/Executions/UnpauseControllers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Controllers;
+using UnityEngine;
+
+namespace Assets.Model.GameStates.Executions
+{
+    [CreateAssetMenu(menuName = "GameStates/Executions/Unpause Controllers")]
+    public class UnpauseControllers : Execution
+    {
+        public override void Execute(StateMachine machine, float timeDelta)
+        {
+            TerrainController.Instance.enabled = true;
+            PlayerController.Instance.enabled = true;
+            CameraController.Instance.enabled = true;
+        }
+    }
+}
