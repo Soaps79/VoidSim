261f974459e67f5f657f6849650e6060876d7e64
Ryan Little <TacticSiege@gmail.com>
2017-03-22 00:13:54 +0000

    updating player movement during selection state
    git-tfs-id: [https://tacticsiege.visualstudio.com/DefaultCollection]$/VoidSim/develop;C427

--- src/SpaceDwarf/Assets/Controllers/CameraController.cs
+++ src/SpaceDwarf/Assets/Controllers/CameraController.cs
@@ -6,24 +6,50 @@ namespace Assets.Controllers
 {
     public class CameraController : SingletonBehavior<CameraController>
     {
-        public GameObject MainCamera;
-        public Camera ActiveCamera { get { return MainCamera.GetComponent<Camera>(); } }
+        private Camera _activeCamera;
+        private Camera _prevCamera;
+        public Camera ActiveCamera { get { return _activeCamera; } }
+
+        public Camera FreeCamera;
+        public Camera FollowCamera;
 
         protected override void OnStart()
         {
             base.OnStart();
+            FreeCamera.enabled = false;
+            FollowCamera.enabled = true;
+            _prevCamera = _activeCamera =  FollowCamera; 
         }
 
-        public void ChangeCamera(GameObject cameraObject)
+        public void ChangeCamera(Camera cameraObject)
         {
+            if (cameraObject == FreeCamera)
+            {
+                FreeCamera.enabled = true;
+                FollowCamera.enabled = false;
+                _prevCamera = _activeCamera;
+                _activeCamera = FreeCamera;
+            }
+            else if (cameraObject == FollowCamera)
+            {
+                FreeCamera.enabled = false;
+                FollowCamera.enabled = true;
+                _prevCamera = _activeCamera;
+                _activeCamera = FollowCamera;
+            }
+            else
+            {
+                Debug.LogWarning("Switching to unrecognized camera. Disabling Camera Controller registered cameras.");
+                FreeCamera.enabled = false;
+                FollowCamera.enabled = false;
+                cameraObject.enabled = true;
+                _prevCamera = _activeCamera;
+                _activeCamera = cameraObject;
+            }
         }
         public void Revert()
         {
-        }
-
-        private void SwapCamera(GameObject nextCameraObject)
-        {
-            
+            ChangeCamera(_prevCamera);
         }
     }
 }
---
+++ src/SpaceDwarf/Assets/Model/GameStates/Executions/ChangePlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Controllers;
+using UnityEngine;
+
+namespace Assets.Model.GameStates.Executions
+{
+    [CreateAssetMenu(menuName = "GameStates/Executions/Change Player Movement")]
+    public class ChangePlayerMovement : Execution
+    {
+        public bool CanMove = true;
+
+        public override void Execute(StateMachine machine, float timeDelta)
+        {
+            PlayerController.Instance.PlayerCharacter.CanMove = CanMove;
+        }
+    }
+}
