7143edf12f9a8647be1619e851c918f65fb8244f
Ryan Little <TacticSiege@gmail.com>
2017-03-22 02:37:58 +0000

    Bug fix for Resize panel, resize like windows
    git-tfs-id: [https://tacticsiege.visualstudio.com/DefaultCollection]$/VoidSim/develop;C431

--- src/SpaceDwarf/Assets/Controllers/GUI/ResizePanel.cs
+++ src/SpaceDwarf/Assets/Controllers/GUI/ResizePanel.cs
@@ -6,7 +6,6 @@ namespace Assets.Controllers.GUI
 {
     public class ResizePanel : OrderedEventBehavior, IPointerDownHandler, IDragHandler
     {
-
         public Vector2 MinSize = new Vector2(30, 40);
         public Vector2 MaxSize = new Vector2(1920, 1080);
 
@@ -31,9 +30,26 @@ namespace Assets.Controllers.GUI
                 return;
 
             var sizeDelta = _transform.sizeDelta;
-
             RectTransformUtility.ScreenPointToLocalPointInRectangle(_transform, data.position, data.pressEventCamera, out _currentPointer);
+
             var resizeValue = _currentPointer - _previousPointer;
+
+            // only resize if we are at the extreme bottom or right, minus a modest buffer zone
+            const int buffer = 20;
+            var nearBottom = _currentPointer.y < _transform.rect.position.y - buffer;
+            if (!nearBottom)
+            {
+                // not near the bottom, don't allow negative y resize
+                resizeValue.y = Mathf.Max(resizeValue.y, 0);
+            }
+            var nearRight = _currentPointer.x > _transform.rect.position.x + _transform.rect.width - buffer;
+            if (!nearRight)
+            {
+                // not near the right, don't allow positive x resize
+                resizeValue.x = Mathf.Min(resizeValue.x, 0);
+            }
+
+            //Debug.Log(string.Format("NearBottom:{0}; NearRight:{1}", nearBottom, nearRight));
 
             sizeDelta += new Vector2(resizeValue.x, -resizeValue.y);
             sizeDelta = new Vector2(
