020c4db4a42e3ec49457a52f1cb7271767526c4d
Daniel Sopel <dsopel@hotmail.com>
2017-04-15 17:42:08 +0000

    Product trading first pass
    git-tfs-id: [https://tacticsiege.visualstudio.com/DefaultCollection]$/VoidSim/develop;C514

---
+++ src/VoidSim/Assets/WorldMaterials/ProductTrader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using QGame;
+
+namespace Assets.WorldMaterials
+{
+    /// <summary>
+    /// Actors will use one of these objects to manage their participation in the galaxy's trading system
+    /// </summary>
+    public class ProductTrader : QScript
+    {
+        public const string MessageName = "TraderInstance";
+
+        public readonly List<ProductTradeRequest> Providing = new List<ProductTradeRequest>();
+        public readonly List<ProductTradeRequest> Consuming = new List<ProductTradeRequest>();
+
+        public Action<int, int> OnProvideSucess;
+        public Action<int, int> OnConsumeSucess;
+
+        public void HandleProvideSuccess(int productId, int amount)
+        {
+            if (OnProvideSucess != null)
+                OnProvideSucess(productId, amount);
+        }
+
+        public void HandleConsumeSuccess(int productId, int amount)
+        {
+            if (OnConsumeSucess != null)
+                OnConsumeSucess(productId, amount);
+        }
+    }
+}
---
+++ src/VoidSim/Assets/WorldMaterials/ProductTradingHub.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Messaging;
+using QGame;
+using UnityEngine;
+
+namespace Assets.WorldMaterials
+{
+    public class TraderInstanceMessageArgs : MessageArgs
+    {
+        public ProductTrader Trader;
+    }
+
+    public class ProductTradeRequest
+    {
+        public int ProductId;
+        public int Amount;
+    }
+
+    /// <summary>
+    /// Manages the supply and demand of Products between game actors. 
+    /// Traders are added through messaging, can expose requests to provide or consume
+    /// 
+    /// Will need work to handle currency
+    /// </summary>
+    public class ProductTradingHub : QScript, IMessageListener
+    {
+        private readonly List<ProductTrader> _traders = new List<ProductTrader>();
+
+        void Start()
+        {
+            MessageHub.Instance.AddListener(this, ProductTrader.MessageName);
+        }
+
+        public void HandleMessage(string type, MessageArgs args)
+        {
+            if (type == ProductTrader.MessageName && args != null)
+                HandleTraderAdd(args as TraderInstanceMessageArgs);
+        }
+
+        private void HandleTraderAdd(TraderInstanceMessageArgs args)
+        {
+            if(args == null || args.Trader == null)
+                throw new UnityException("TradeHub given bad message data");
+
+            if(_traders.All(i => i.name != args.Trader.name))
+                _traders.Add(args.Trader);
+
+            CheckForTrades();
+        }
+
+        private void CheckForTrades()
+        {
+            var providers = _traders.Where(i => i.Providing.Any());
+            var consumers = _traders.Where(i => i.Consuming.Any());
+
+            // will be set to true if any provide or consume request is completed
+            var needsPruning = false;
+
+            // loop through all products provided and see if they have consumers
+            foreach (var provider in providers)
+            {
+                foreach (var provided in provider.Providing)
+                {
+                    var totalAmountConsumed = 0;
+
+                    foreach (var consumer in consumers)
+                    {
+                        // only consider consumers who want this product
+                        if (consumer.name == provider.name
+                            || consumer.Consuming.All(i => i.ProductId != provided.ProductId)) continue;
+                        
+                        var amountConsumed = 0;
+                        var consumed = consumer.Consuming.First(i => i.ProductId == provided.ProductId);
+
+                        // consumer either finishes off entire amount
+                        if (consumed.Amount > provided.Amount)
+                        {
+                            amountConsumed = provided.Amount;
+                            consumed.Amount -= amountConsumed;
+                            provided.Amount = 0;
+                            needsPruning = true;
+                        }
+                        // or takes its fill
+                        else
+                        {
+                            amountConsumed = consumed.Amount;
+                            provided.Amount -= amountConsumed;
+                            consumed.Amount = 0;
+                            needsPruning = true;
+                        }
+
+                        // tell the consumer it got its goods
+                        if(amountConsumed >= 0)
+                            consumer.HandleConsumeSuccess(provided.ProductId, amountConsumed);
+
+                        // move on to the next consumer, break if provider has emptied its stock
+                        totalAmountConsumed += amountConsumed;
+                        if (totalAmountConsumed >= provided.Amount) break;
+                    }
+                    
+                    // only tell the provider once how much has been consumed
+                    if (totalAmountConsumed > 0)
+                    {
+                        provided.Amount -= totalAmountConsumed;
+                        provider.HandleProvideSuccess(provided.ProductId, totalAmountConsumed);
+                    }
+
+                }
+            }
+
+            if (needsPruning) PruneTraderList();
+        }
+
+        private void PruneTraderList()
+        {
+            foreach (var trader in _traders)
+            {
+                trader.Consuming.RemoveAll(i => i.Amount <= 0);
+                trader.Providing.RemoveAll(i => i.Amount <= 0);
+            }
+        }
+
+        public string Name { get { return "ProductTradingHub"; } }
+    }
+}
