2538c5b48961dcd3e1cb0c485e272b18019fefc2
Daniel Sopel <dsopel@hotmail.com>
2017-04-17 01:29:18 +0000

    Odd inheritance problem example
    git-tfs-id: [https://tacticsiege.visualstudio.com/DefaultCollection]$/VoidSim/develop;C515

---
+++ src/VoidSim/Assets/Editor/ProductTradingHubTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Assets.Scripts.Testing;
+using Assets.WorldMaterials;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    public class ProductTradingHubTests : ProductTradingHub
+    {
+        private GameObject _gameObject;
+        private ProductTradingHub _target;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _gameObject = new GameObject(string.Format("TestObject-{0}", DateTime.Now.Millisecond));
+            _gameObject.AddComponent<ProductTradingHub>();
+
+            _target = _gameObject.GetComponent<ProductTradingHub>();
+            Assert.IsNotNull(_target);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _gameObject = null;
+            _target = null;
+        }
+
+        [Test]
+        public void TryAddConsumer()
+        {
+            Assert.IsTrue(true);
+        }
+
+    }
+}
