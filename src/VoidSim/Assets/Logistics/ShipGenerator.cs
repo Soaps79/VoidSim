105558d8474e833d61c4fa51bbb4258d32675177
dansopel <dansopel@gmail.com>
2017-06-12 21:52:49 -0400

    ShipGenerator saving and loading

--- src/VoidSim/Assets/Logistics/ShipGenerator.cs
+++ src/VoidSim/Assets/Logistics/ShipGenerator.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Assets.Logistics.Ships;
 using Assets.Scripts.Serialization;
 using Messaging;
@@ -8,12 +8,26 @@ using UnityEngine;
 
 namespace Assets.Logistics
 {
+	public class ShipGeneratorData
+	{
+		public List<QueuedShipData> Ships;
+		public int LastId;
+	}
+
+	public class QueuedShipData
+	{
+		public string ShipName;
+		public float TimeRemaining;
+	}
+
 	/// <summary>
-	/// Handles creation of ships, initializing their navigation
+	/// Handles creation of ships, initializing their navigation. Currently used to launch 
+	/// a bunch of ships into the world from a starting list, will eventually make more sense
 	/// </summary>
 	[RequireComponent(typeof(TransitControl))]
-	public class ShipGenerator : QScript, IMessageListener
+	public class ShipGenerator : QScript, IMessageListener, ISerializeData<ShipGeneratorData>
 	{
+		// will eventually be used to store type of ship
 		public class Entry
 		{
 			public string ShipName;
@@ -25,21 +39,36 @@ namespace Assets.Logistics
 		[SerializeField] private GameObject _cargoShip;
 		[SerializeField] private ShipSchedule _schedule;
 
-		private List<Entry> _toLaunch = new List<Entry>();
+		private readonly CollectionSerializer<ShipGeneratorData> _serializer
+			= new CollectionSerializer<ShipGeneratorData>();
+
+		private readonly List<Entry> _toLaunch = new List<Entry>();
 
 		void Start()
 		{
+			// currently listens to own message, and if ship is new, 'kicks' it
+			// may not be needed anymore?
 			MessageHub.Instance.AddListener(this, LogisticsMessages.ShipCreated);
 			_transitControl = gameObject.GetComponent<TransitControl>();
 
-			if (!SerializationHub.Instance.IsLoading)
+			if (_serializer.HasDataFor(this, "ShipGenerator"))
+				LoadSerializedShips();
+			else
+				LoadInitialShips();
+		}
+
+		// pulls data from serializer and queues ships
+		private void LoadSerializedShips()
+		{
+			var data = _serializer.DeserializeData();
+			_lastShipId = data.LastId;
+			foreach (var shipData in data.Ships)
 			{
-				// give locations some time to register
-				var node = StopWatch.AddNode("begin", 1, true);
-				node.OnTick += LoadInitialShips;
+				QueueShip(shipData.ShipName, shipData.TimeRemaining);
 			}
 		}
 
+		// pulls starting schedule from SO and queues ships
 		private void LoadInitialShips()
 		{
 			if (_schedule == null)
@@ -52,17 +81,24 @@ namespace Assets.Logistics
 			{
 				_lastShipId++;
 				var shipName = "ship_" + _lastShipId;
-				var node = StopWatch.AddNode(shipName, entry.InitialDelay, true);
-				var e = new Entry
-				{
-					ShipName = shipName,
-					Node = node
-				};
-				node.OnTick += () => CreateShip(e);
-				_toLaunch.Add(e);
+				QueueShip(shipName, entry.InitialDelay);
 			}
 		}
 
+		// begins a timer that will launch the ship when it is complete
+		private void QueueShip(string shipName, float time)
+		{
+			var node = StopWatch.AddNode(shipName, time, true);
+			var e = new Entry
+			{
+				ShipName = shipName,
+				Node = node
+			};
+			node.OnTick += () => CreateShip(e);
+			_toLaunch.Add(e);
+		}
+
+		// fills a navigation object with all known locations and instantiates a ship
 		private void CreateShip(Entry entry)
 		{
 			var locations = _transitControl.GetTransitLocations();
@@ -79,6 +115,7 @@ namespace Assets.Logistics
 			MessageHub.Instance.QueueMessage(LogisticsMessages.ShipCreated, new ShipCreatedMessageArgs { Ship = ship });
 		}
 
+		// not sure if still necessary to wait a cycle?
 		public void HandleMessage(string type, MessageArgs args)
 		{
 			if (type == LogisticsMessages.ShipCreated && args != null)
@@ -94,5 +131,18 @@ namespace Assets.Logistics
 		}
 
 		public string Name { get { return name; } }
+
+		public ShipGeneratorData GetData()
+		{
+			return new ShipGeneratorData
+			{
+				LastId = _lastShipId,
+				Ships = _toLaunch.Select(i => new QueuedShipData
+				{
+					ShipName = i.ShipName,
+					TimeRemaining = i.Node.RemainingLifetime
+				}).ToList()
+			};
+		}
 	}
 }
