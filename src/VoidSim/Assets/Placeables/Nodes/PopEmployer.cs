405dbb01050c3bfb9e1cd53738961ec714f9d8d4
dansopel <dansopel@gmail.com>
2017-08-06 22:54:28 -0400

    PopEmployer node created, PopControl now tracking them

---
+++ src/VoidSim/Assets/Placeables/Nodes/PopEmployer.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts;
+using Messaging;
+
+namespace Assets.Placeables.Nodes
+{
+	public class PopEmployerMessageArgs : MessageArgs
+	{
+		public PopEmployer PopEmployer;
+	}
+
+	public class PopEmployer : PlaceableNode
+	{
+		public const string MessageName = "PopEmployerCreated";
+		public int EmployeeCount;
+
+		public override void BroadcastPlacement()
+		{
+			Locator.MessageHub.QueueMessage(MessageName, new PopEmployerMessageArgs { PopEmployer = this });
+		}
+
+		public override string NodeName { get { return "PopEmployer"; } }
+	}
+}
--- src/VoidSim/Assets/Station/PopulationControl.cs
+++ src/VoidSim/Assets/Station/PopulationControl.cs
@@ -13,126 +13,141 @@ using UnityEngine;
 
 namespace Assets.Station
 {
-    /// <summary>
-    /// Placeholder for where pop will be managed
-    /// </summary>
-    public class PopulationControl : QScript, IPopulationHost, ITraderDriver, IMessageListener
-    {
-        [SerializeField] private int _totalCapacity;
-        [SerializeField] private int _initialCapacity;
-        private readonly List<PopHousing> _housing = new List<PopHousing>();
-        private const string POPULATION_PRODUCT_NAME = "Population";
-        private Inventory _inventory;
-        private int _populationProductId;
+	/// <summary>
+	/// Placeholder for where pop will be managed
+	/// </summary>
+	public class PopulationControl : QScript, IPopulationHost, ITraderDriver, IMessageListener
+	{
+		[SerializeField] private int _totalCapacity;
+		[SerializeField] private int _initialCapacity;
+		private readonly List<PopHousing> _housing = new List<PopHousing>();
+		private readonly List<PopEmployer> _employers = new List<PopEmployer>();
+		private const string POPULATION_PRODUCT_NAME = "Population";
+		private Inventory _inventory;
+		private int _populationProductId;
 		// When there is room for more population, it it requested through this Trader which is hooked into the trade system
-	    private ProductTrader _trader;
-	    private int _inboundPopulation;
+		private ProductTrader _trader;
+		private int _inboundPopulation;
 
-        public void Initialize(Inventory inventory, int initialCapacity = 0)
-        {
-            _initialCapacity = initialCapacity;
-            _inventory = inventory;
-	        _inventory.OnProductsChanged += HandleInventoryProductChanged;
+		public void Initialize(Inventory inventory, int initialCapacity = 0)
+		{
+			_initialCapacity = initialCapacity;
+			_inventory = inventory;
+			_inventory.OnProductsChanged += HandleInventoryProductChanged;
 
-            var pop = ProductLookup.Instance.GetProduct(POPULATION_PRODUCT_NAME);
-            _populationProductId = pop.ID;
-	        CurrentQualityOfLife = 10;
+			var pop = ProductLookup.Instance.GetProduct(POPULATION_PRODUCT_NAME);
+			_populationProductId = pop.ID;
+			CurrentQualityOfLife = 10;
 
-            if (_initialCapacity > 0)
-                _inventory.SetProductMaxAmount(_populationProductId, _initialCapacity);
+			if (_initialCapacity > 0)
+				_inventory.SetProductMaxAmount(_populationProductId, _initialCapacity);
 
 			Locator.MessageHub.AddListener(this, PopHousing.MessageName);
+			Locator.MessageHub.AddListener(this, PopEmployer.MessageName);
 
 			// remove when housing serialization is in place
 			Locator.LastId.Reset("pop_housing");
 
-	        InitializeProductTrader();
-        }
+			InitializeProductTrader();
+		}
 
-	    private void HandleInventoryProductChanged(int productId, int amount)
-	    {
-		    if (productId == _populationProductId)
-			    _inboundPopulation -= amount;
-	    }
+		private void HandleInventoryProductChanged(int productId, int amount)
+		{
+			if (productId == _populationProductId)
+				_inboundPopulation -= amount;
+		}
 
-	    private void InitializeProductTrader()
-	    {
-		    _trader = gameObject.AddComponent<ProductTrader>();
+		private void InitializeProductTrader()
+		{
+			_trader = gameObject.AddComponent<ProductTrader>();
 			_trader.Initialize(this, Station.ClientName);
-		    UpdateTradeRequest();
-	    }
+			UpdateTradeRequest();
+		}
 
 		// checks to see if inventory has room for more pop (discounting those already in transit)
-	    private void UpdateTradeRequest()
-	    {
-		    var remaining = _inventory.GetProductRemainingSpace(_populationProductId);
-		    remaining -= _inboundPopulation;
-		    _trader.SetConsume(new ProductAmount
-		    {
-			    ProductId = _populationProductId,
+		private void UpdateTradeRequest()
+		{
+			var remaining = _inventory.GetProductRemainingSpace(_populationProductId);
+			remaining -= _inboundPopulation;
+			_trader.SetConsume(new ProductAmount
+			{
+				ProductId = _populationProductId,
 				Amount = remaining > 0  ? remaining : 0
-		    });
-	    }
+			});
+		}
 
-	    public int TotalCapacity
-        {
-            get { return _totalCapacity; }
-        }
+		public int TotalCapacity
+		{
+			get { return _totalCapacity; }
+		}
 
-        public void HandleMessage(string type, MessageArgs args)
-        {
-            if (type == PopHousing.MessageName)
-                HandleHousingAdd(args as PopHousingMessageArgs);
-        }
+		public void HandleMessage(string type, MessageArgs args)
+		{
+			if (type == PopHousing.MessageName)
+				HandleHousingAdd(args as PopHousingMessageArgs);
+			else if (type == PopEmployer.MessageName)
+				HandleEmployerAdd(args as PopEmployerMessageArgs);
+		}
 
-        private void HandleHousingAdd(PopHousingMessageArgs args)
-        {
-            if (args == null || args.PopHousing == null)
-            {
-                Debug.Log("PopulationControl given bad consumer message args.");
-                return;
-            }
+		private void HandleHousingAdd(PopHousingMessageArgs args)
+		{
+			if (args == null || args.PopHousing == null)
+			{
+				Debug.Log("PopulationControl given bad housing message args.");
+				return;
+			}
 
-	        args.PopHousing.name = "pop_housing_" + Locator.LastId.GetNext("pop_housing");
+			args.PopHousing.name = "pop_housing_" + Locator.LastId.GetNext("pop_housing");
 
-            _housing.Add(args.PopHousing);
-            UpdateCapacity();
+			_housing.Add(args.PopHousing);
+			UpdateCapacity();
 			UpdateTradeRequest();
-        }
+		}
 
-        private void UpdateCapacity()
-        {
-            _totalCapacity = _initialCapacity + _housing.Sum(i => i.Capacity);
-	        _inventory.SetProductMaxAmount(_populationProductId, _totalCapacity);
+		private void HandleEmployerAdd(PopEmployerMessageArgs args)
+		{
+			if (args == null || args.PopEmployer == null)
+			{
+				Debug.Log("PopulationControl given bad employer message args.");
+				return;
+			}
+
+			_employers.Add(args.PopEmployer);
+		}
+
+		private void UpdateCapacity()
+		{
+			_totalCapacity = _initialCapacity + _housing.Sum(i => i.Capacity);
+			_inventory.SetProductMaxAmount(_populationProductId, _totalCapacity);
 		}
 
 		public string Name
-        {
-            get { return "PopulationControl"; }
-        }
+		{
+			get { return "PopulationControl"; }
+		}
 
-	    public float CurrentQualityOfLife { get; private set; }
-	    public bool PopulationWillMigrateTo(IPopulationHost otherHost)
-	    {
-		    return false;
-	    }
+		public float CurrentQualityOfLife { get; private set; }
+		public bool PopulationWillMigrateTo(IPopulationHost otherHost)
+		{
+			return false;
+		}
 
-	    public bool WillConsumeFrom(ProductTrader provider, ProductAmount provided)
-	    {
+		public bool WillConsumeFrom(ProductTrader provider, ProductAmount provided)
+		{
 			if(provided.ProductId != _populationProductId)
 				throw new UnityException("PopulationControl offered trade that was not population");
 
-		    _inboundPopulation += provided.Amount;
-		    return true;
-	    }
-
-	    public bool WillProvideTo(ProductTrader consumer, ProductAmount provided)
-	    {
+			_inboundPopulation += provided.Amount;
 			return true;
 		}
 
-	    public void HandleProvideSuccess(TradeManifest manifest)
-	    {
+		public bool WillProvideTo(ProductTrader consumer, ProductAmount provided)
+		{
+			return true;
+		}
+
+		public void HandleProvideSuccess(TradeManifest manifest)
+		{
 			Locator.MessageHub.QueueMessage(LogisticsMessages.CargoRequested, new CargoRequestedMessageArgs
 			{
 				Manifest = new CargoManifest(manifest)
@@ -145,9 +160,9 @@ namespace Assets.Station
 			});
 		}
 
-	    public void HandleConsumeSuccess(TradeManifest manifest)
-	    {
-		    
-	    }
-    }
+		public void HandleConsumeSuccess(TradeManifest manifest)
+		{
+			
+		}
+	}
 }
