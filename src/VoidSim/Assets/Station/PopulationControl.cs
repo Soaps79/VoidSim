d9b45a9e5445f0ac9cf2fa2ac0bbac682f6acbc5
dansopel <dansopel@gmail.com>
2017-08-21 22:15:18 -0400

    EmployerControl extracted from PopControl

--- src/VoidSim/Assets/Station/PopulationControl.cs
+++ src/VoidSim/Assets/Station/PopulationControl.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Assets.Logistics;
 using Assets.Placeables.Nodes;
 using Assets.Scripts;
 using Assets.Scripts.Serialization;
-using Assets.Station.Efficiency;
 using Assets.WorldMaterials;
 using Assets.WorldMaterials.Population;
 using Assets.WorldMaterials.Products;
@@ -13,23 +11,15 @@ using Assets.WorldMaterials.Trade;
 using Messaging;
 using QGame;
 using UnityEngine;
-using Random = UnityEngine.Random;
-using TimeLength = Assets.Scripts.TimeLength;
 
 namespace Assets.Station
 {
 	public class PopulationData
 	{
 		public int CurrentCount;
-		public int CurrentUnemployed;
 		public int Capacity;
-		public List<EmployerData> Employers;
-	}
-
-	public class EmployerData
-	{
-		public string EmployerName;
-		public int Count;
+		public int InboundPopulation;
+		public EmployerControlData EmployerControlData;
 	}
 
 	/// <summary>
@@ -42,9 +32,7 @@ namespace Assets.Station
 		[SerializeField] private int _totalCapacity;
 		[SerializeField] private int _baseCapacity;
 		[SerializeField] private int _currentCount;
-		[SerializeField] private int _currentUnemployed;
 		private readonly List<PopHousing> _housing = new List<PopHousing>();
-		private readonly List<PopEmployer> _employers = new List<PopEmployer>();
 
 		// Population is currently stored in the Station's inventory as a Product
 		// Allows it to be handled by trade, cargo, etc
@@ -55,65 +43,51 @@ namespace Assets.Station
 		// it is requested through this Trader which is hooked into the trade system
 		private ProductTrader _trader;
 		private int _inboundPopulation;
-		
-		// basic employment model
-		[SerializeField] private TimeLength _employmentUpdateTimeLength;
-		[SerializeField] private int _employmentUpdateCount;
-		private readonly string _stopwatchNodeName = "employment";
-		private float _baseEmployChance;
-
-		public MoodManager MoodManager { get; private set; }
-
-		// passed on to Employers
-		private readonly EfficiencyAffector _employerAffector = new EfficiencyAffector("Pop Mood");
 
 		private PopulationSO _scriptable;
 		private bool _ignoreNeeds;
 
+		public MoodManager MoodManager { get; private set; }
+		private EmployerControl _employerControl;
+
+		// serialization
 		private readonly CollectionSerializer<PopulationData> _serializer
 			= new CollectionSerializer<PopulationData>();
 		private PopulationData _deserialized;
-		// will go away when pop has a serialized model
-		private int _handledDeserializedCount;
-
 		private const string _collectionName = "PopulationControl";
+
 
 		public void Initialize(Inventory inventory, PopulationSO scriptable)
 		{
 			_scriptable = scriptable;
-			MoodManager = new MoodManager(scriptable.MoodParams, inventory);
-			MoodManager.EfficiencyModule.OnValueChanged += HandleMoodChange;
-
 			_inventory = inventory;
 			_inventory.OnProductsChanged += HandleInventoryProductChanged;
 
 			_populationProductId = ProductIdLookup.Population;
 			_currentCount = _inventory.GetProductCurrentAmount(_populationProductId);
-			_currentUnemployed = _currentCount;
-
-			// still temporary values while system is worked out
-			CurrentQualityOfLife = 10;
-
 			_baseCapacity = scriptable.BaseCapacity;
 
+			// still temporary values until system is worked out
+			CurrentQualityOfLife = 10;
+
+			// establish sub-objects
+			MoodManager = new MoodManager(scriptable.MoodParams, inventory);
+			_employerControl = gameObject.AddComponent<EmployerControl>();
+			_employerControl.Initialize(scriptable, MoodManager.EfficiencyModule, _currentCount);
+
+			// load or set defaults
 			if (_serializer.HasDataFor(this, _collectionName))
 				LoadFromFile();
 			else
-				LoadFromScriptable(scriptable);
-
+				LoadFromScriptable();
 			_inventory.SetProductMaxAmount(_populationProductId, scriptable.BaseCapacity);
 
 			Locator.MessageHub.AddListener(this, PopHousing.MessageName);
-			Locator.MessageHub.AddListener(this, PopEmployer.MessageName);
-
-			// remove when housing serialization is in place
-			//Locator.LastId.Reset("pop_housing");
 
 			InitializeProductTrader();
-			InitializeEmploymentUpdate(_scriptable.EmploymentParams);
 		}
 
-		private void LoadFromScriptable(PopulationSO scriptable)
+		private void LoadFromScriptable()
 		{
 			_inventory.TryAddProduct(_populationProductId, _scriptable.InitialCount);
 		}
@@ -121,57 +95,11 @@ namespace Assets.Station
 		private void LoadFromFile()
 		{
 			_deserialized = _serializer.DeserializeData();
-			_currentUnemployed = _deserialized.CurrentUnemployed;
 			_currentCount = _deserialized.CurrentCount;
+			_inboundPopulation = _deserialized.InboundPopulation;
+			_employerControl.Deserialize(_deserialized.EmployerControlData);
 			if(_inventory.GetProductCurrentAmount(ProductIdLookup.Population) != _currentCount)
 				throw new UnityException("PopControl data not matching station inventory");
-		}
-
-		private void HandleMoodChange(EfficiencyModule module)
-		{
-			_employerAffector.Efficiency = module.CurrentAmount;
-		}
-
-		// register with stopwatch to regularly check for updates
-		private void InitializeEmploymentUpdate(EmploymentParams @params)
-		{
-			_employmentUpdateTimeLength = @params.EmploymentUpdateTimeLength;
-			_employmentUpdateCount = @params.EmploymentUpdateCount;
-			_baseEmployChance = @params.BaseEmployChance;
-
-			var time = Locator.WorldClock.GetSeconds(_employmentUpdateTimeLength);
-			var node = StopWatch.AddNode(_stopwatchNodeName, time);
-			node.OnTick += HandleEmploymentUpdate;
-		}
-
-		private void HandleEmploymentUpdate()
-		{
-			if (_currentUnemployed <= 0 || !_employers.Any(i => i.HasRoom))
-				return;
-
-			// making a queue as basic distribution
-			var seeking = _employers.Where(i => i.HasRoom).OrderBy(i => i.EmployeeDesirability).ToList();
-			var employers = new Queue<PopEmployer>();
-			seeking.ForEach(i => employers.Enqueue(i));
-
-			// for each possible employee
-			for (int i = 0; i < _employmentUpdateCount; i++)
-			{
-				if (_currentUnemployed < 0 || !employers.Any())
-					break;
-
-				// see if they want the job
-				var roll = Random.value;
-				if (roll > _baseEmployChance)
-					continue;
-
-				// pop the employer, give him the worker, add to back if he still has room
-				var employer = employers.Dequeue();
-				employer.AddEmployee(1);
-				_currentUnemployed -= 1;
-				if(employer.HasRoom)
-					employers.Enqueue(employer);
-			}
 		}
 
 		private void InitializeProductTrader()
@@ -192,7 +120,7 @@ namespace Assets.Station
 				_inboundPopulation -= amount;
 
 			_currentCount = _inventory.GetProductCurrentAmount(_populationProductId);
-			_currentUnemployed += amount;
+			_employerControl.CurrentUnemployed += amount;
 		}
 
 		// checks to see if inventory has room for more pop (discounting those already in transit)
@@ -207,16 +135,12 @@ namespace Assets.Station
 			});
 		}
 
-		// subscribed to messages for housing and employment being placed
 		public void HandleMessage(string type, MessageArgs args)
 		{
 			switch (type)
 			{
 				case PopHousing.MessageName:
 					HandleHousingAdd(args as PopHousingMessageArgs);
-					break;
-				case PopEmployer.MessageName:
-					HandleEmployerAdd(args as PopEmployerMessageArgs);
 					break;
 			}
 		}
@@ -229,65 +153,22 @@ namespace Assets.Station
 				return;
 			}
 
+			// name it since popcontrol owns all pop housings
 			args.PopHousing.name = _placeableNameSuffix + Locator.LastId.GetNext("pop_housing");
-
 			_housing.Add(args.PopHousing);
+
+			// update capacity, put out a request for inhabitants
 			UpdateCapacity();
 			UpdateTradeRequest();
 		}
-
+		
 		private void UpdateCapacity()
 		{
 			_totalCapacity = _baseCapacity + _housing.Sum(i => i.Capacity);
 			_inventory.SetProductMaxAmount(_populationProductId, _totalCapacity);
 		}
 
-		private void HandleEmployerAdd(PopEmployerMessageArgs args)
-		{
-			if (args == null || args.PopEmployer == null)
-			{
-				Debug.Log("PopulationControl given bad employer message args.");
-				return;
-			}
-
-			var employer = args.PopEmployer;
-			employer.RegisterMood(_employerAffector);
-			_employers.Add(employer);
-
-			if (_deserialized != null && _deserialized.Employers.Any(i => i.EmployerName == employer.name))
-				HandleExistingEmployer(employer);
-			else
-				HandleNewEmployer(employer);
-		}
-
-		private void HandleExistingEmployer(PopEmployer employer)
-		{
-			var data = _deserialized.Employers.First(i => i.EmployerName == employer.name);
-			employer.AddEmployee(data.Count);
-		}
-
-		private void HandleNewEmployer(PopEmployer employer)
-		{
-			// real basic implementation of placing employees, place half the max amount
-			if (_currentUnemployed > 0)
-			{
-				var countToEmploy = employer.MaxEmployeeCount / 2;
-				if (_currentUnemployed > countToEmploy)
-				{
-					employer.AddEmployee(countToEmploy);
-					_currentUnemployed -= countToEmploy;
-				}
-				else
-				{
-					employer.CurrentEmployeeCount = _currentUnemployed;
-					_currentUnemployed = 0;
-				}
-			}
-		}
-
-
-		public float CurrentQualityOfLife { get; private set; }
-
+		// will cause mood to be ignored, always returning 100%
 		public bool IgnoreNeeds
 		{
 			get { return _ignoreNeeds; }
@@ -301,11 +182,14 @@ namespace Assets.Station
 			}
 		}
 
+		public float CurrentQualityOfLife { get; private set; }
+
 		public bool PopulationWillMigrateTo(IPopulationHost otherHost)
 		{
 			return false;
 		}
 
+		// since there are people en-route, remove them from current trade request
 		public bool WillConsumeFrom(ProductTrader provider, ProductAmount provided)
 		{
 			if(provided.ProductId != _populationProductId)
@@ -323,6 +207,7 @@ namespace Assets.Station
 			return true;
 		}
 
+		// request cargo passage for pop
 		public void HandleProvideSuccess(TradeManifest manifest)
 		{
 			Locator.MessageHub.QueueMessage(LogisticsMessages.CargoRequested, new CargoRequestedMessageArgs
@@ -338,19 +223,15 @@ namespace Assets.Station
 		}
 
 		public void HandleConsumeSuccess(TradeManifest manifest) { }
+
 		public PopulationData GetData()
 		{
 			var data = new PopulationData
 			{
 				CurrentCount = _currentCount,
-				CurrentUnemployed = _currentUnemployed,
 				Capacity = _totalCapacity,
-				Employers = _employers.Select(i => 
-					new EmployerData
-					{
-						EmployerName = i.name,
-						Count = i.CurrentEmployeeCount
-					}).ToList()
+				InboundPopulation = _inboundPopulation,
+				EmployerControlData = _employerControl.GetData()
 			};
 			return data;
 		}
---
+++ src/VoidSim/Assets/WorldMaterials/Population/EmployerControl.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Placeables.Nodes;
+using Assets.Scripts;
+using Assets.Scripts.Serialization;
+using Assets.Station;
+using Assets.Station.Efficiency;
+using Messaging;
+using QGame;
+using UnityEngine;
+using TimeLength = Assets.Scripts.TimeLength;
+
+namespace Assets.WorldMaterials.Population
+{
+	public class EmployerControlData
+	{
+		public int CurrentUnemployed;
+		public List<EmployerData> Employers;
+	}
+
+	public class EmployerData
+	{
+		public string EmployerName;
+		public int Count;
+	}
+
+	/// <summary>
+	/// Handles matching population with employment
+	/// </summary>
+	public class EmployerControl : QScript, ISerializeData<EmployerControlData>, IMessageListener
+	{
+		public int CurrentUnemployed;
+
+		[SerializeField] private TimeLength _employmentUpdateTimeLength;
+		[SerializeField] private int _employmentUpdateCount;
+		private float _baseEmployChance;
+
+		private readonly string _stopwatchNodeName = "employment";
+		private readonly List<PopEmployer> _employers = new List<PopEmployer>();
+		private readonly List<EmployerData> _deserialized = new List<EmployerData>();
+
+		// passed on to Employers
+		private readonly EfficiencyAffector _employerAffector = new EfficiencyAffector("Pop Mood");
+
+		public void Initialize(PopulationSO scriptable, EfficiencyModule module, int currentEmployed)
+		{
+			CurrentUnemployed = currentEmployed;
+			InitializeEmploymentUpdate(scriptable.EmploymentParams);
+			Locator.MessageHub.AddListener(this, PopEmployer.MessageName);
+			if(module == null)
+				throw new UnityException("EmployerControl recieved bad module");
+
+			module.OnValueChanged += HandleMoodChange;
+		}
+
+		private void HandleMoodChange(EfficiencyModule module)
+		{
+			_employerAffector.Efficiency = module.CurrentAmount;
+		}
+
+
+		public void Deserialize(EmployerControlData deserialized)
+		{
+			CurrentUnemployed = deserialized.CurrentUnemployed;
+			_deserialized.AddRange(deserialized.Employers);
+		}
+
+		// register with stopwatch to regularly check for updates
+		private void InitializeEmploymentUpdate(EmploymentParams @params)
+		{
+			_employmentUpdateTimeLength = @params.EmploymentUpdateTimeLength;
+			_employmentUpdateCount = @params.EmploymentUpdateCount;
+			_baseEmployChance = @params.BaseEmployChance;
+
+			var time = Locator.WorldClock.GetSeconds(_employmentUpdateTimeLength);
+			var node = StopWatch.AddNode(_stopwatchNodeName, time);
+			node.OnTick += HandleEmploymentUpdate;
+		}
+
+		private void HandleEmploymentUpdate()
+		{
+			if (CurrentUnemployed <= 0 || !_employers.Any(i => i.HasRoom))
+				return;
+
+			// making a queue as basic distribution
+			var seeking = _employers.Where(i => i.HasRoom).OrderBy(i => i.EmployeeDesirability).ToList();
+			var employers = new Queue<PopEmployer>();
+			seeking.ForEach(i => employers.Enqueue(i));
+
+			// for each possible employee
+			for (int i = 0; i < _employmentUpdateCount; i++)
+			{
+				if (CurrentUnemployed < 0 || !employers.Any())
+					break;
+
+				// see if they want the job
+				var roll = Random.value;
+				if (roll > _baseEmployChance)
+					continue;
+
+				// pop the employer, give him the worker, add to back if he still has room
+				var employer = employers.Dequeue();
+				employer.AddEmployee(1);
+				CurrentUnemployed -= 1;
+				if (employer.HasRoom)
+					employers.Enqueue(employer);
+			}
+		}
+
+		public EmployerControlData GetData()
+		{
+			return new EmployerControlData
+			{
+				CurrentUnemployed = CurrentUnemployed,
+				Employers = _employers.Select(i => new EmployerData
+				{
+					EmployerName = i.name,
+					Count = i.CurrentEmployeeCount
+				}).ToList()
+			};
+		}
+
+		public void HandleMessage(string type, MessageArgs args)
+		{
+			if (type == PopEmployer.MessageName && args != null)
+				HandleEmployerPlacement(args as PopEmployerMessageArgs);
+		}
+
+		private void HandleEmployerPlacement(PopEmployerMessageArgs args)
+		{
+			if (args == null || args.PopEmployer == null)
+			{
+				Debug.Log("PopulationControl given bad employer message args.");
+				return;
+			}
+
+			var employer = args.PopEmployer;
+			employer.RegisterMood(_employerAffector);
+			_employers.Add(employer);
+
+			if (_deserialized.Any(i => i.EmployerName == employer.name))
+				HandleExistingEmployer(employer);
+			else
+				HandleNewEmployer(employer);
+		}
+
+		private void HandleExistingEmployer(PopEmployer employer)
+		{
+			var data = _deserialized.First(i => i.EmployerName == employer.name);
+			employer.AddEmployee(data.Count);
+		}
+
+		private void HandleNewEmployer(PopEmployer employer)
+		{
+			// real basic implementation of placing employees, place half the max amount
+			if (CurrentUnemployed > 0)
+			{
+				var countToEmploy = employer.MaxEmployeeCount / 2;
+				if (CurrentUnemployed > countToEmploy)
+				{
+					employer.AddEmployee(countToEmploy);
+					CurrentUnemployed -= countToEmploy;
+				}
+				else
+				{
+					employer.CurrentEmployeeCount = CurrentUnemployed;
+					CurrentUnemployed = 0;
+				}
+			}
+		}
+
+		public string Name { get { return "EmployerControl"; } }
+	}
+}
