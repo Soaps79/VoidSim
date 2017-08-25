d85d5275473de75150f5139776d3a09f3ad094d6
dansopel <dansopel@gmail.com>
2017-08-24 20:19:51 -0400

    Created main menu scene

---
+++ src/VoidSim/Assets/Scripts/Scenes/SceneController.cs
@@ -0,0 +1,13 @@
+﻿using QGame;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Scenes
+{
+	public class SceneController : QScript
+	{
+		public void OnNewGame()
+		{
+			SceneManager.LoadScene("scene1");
+		}
+	}
+}
