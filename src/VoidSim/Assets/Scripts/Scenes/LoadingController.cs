943eaca741829de3142da2aab92a192ca873bac0
dansopel <dansopel@gmail.com>
2017-08-24 21:41:41 -0400

    Loading bar added

--- src/VoidSim/Assets/Scripts/Scenes/LoadingController.cs
+++ src/VoidSim/Assets/Scripts/Scenes/LoadingController.cs
@@ -1,16 +1,27 @@
-﻿using QGame;
+﻿using Assets.Controllers.GUI;
+using QGame;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Scenes
 {
 	public class LoadingController : QScript
 	{
 		private StopWatchNode _stopwatchNode;
+		[SerializeField] private Slider _slider;
 
 		void Start()
 		{
-			_stopwatchNode = StopWatch.AddNode("loading_bar", 5, true);
-			_stopwatchNode.OnTick += () => SceneManager.LoadScene("scene1");
+			_stopwatchNode = StopWatch.AddNode("loading_bar", 5);
+			_stopwatchNode.OnTick += () =>
+			{
+				_stopwatchNode.Pause();
+				SceneManager.LoadScene("scene1");
+			};
+
+			var binding = _slider.gameObject.AddComponent<SliderBinding>();
+			binding.Initialize( () => _stopwatchNode.RemainingLifetimeAsZeroToOne );
 		}
 	}
 }
