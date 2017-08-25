fef143ea846bca0f6518b6c5ba2241147a28812b
dansopel <dansopel@gmail.com>
2017-08-24 23:26:41 -0400

    Updated loading screen art

--- src/VoidSim/Assets/Scripts/Scenes/LoadingController.cs
+++ src/VoidSim/Assets/Scripts/Scenes/LoadingController.cs
@@ -1,5 +1,7 @@
 ﻿using Assets.Controllers.GUI;
+using DG.Tweening;
 using QGame;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,18 +12,34 @@ namespace Assets.Scripts.Scenes
 	{
 		private StopWatchNode _stopwatchNode;
 		[SerializeField] private Slider _slider;
+		[SerializeField] private TMP_Text _text;
+		private SliderBinding _binding;
+		public float LoadTime;
 
 		void Start()
 		{
-			_stopwatchNode = StopWatch.AddNode("loading_bar", 5);
-			_stopwatchNode.OnTick += () =>
-			{
-				_stopwatchNode.Pause();
-				SceneManager.LoadScene("scene1");
-			};
+			var node = StopWatch.AddNode("loading_bar", LoadTime, true);
+			node.OnTick += HandleLoadComplete;
+			_binding = _slider.gameObject.AddComponent<SliderBinding>();
+			_binding.Initialize(() => node.RemainingLifetimeAsZeroToOne);
+		}
 
-			var binding = _slider.gameObject.AddComponent<SliderBinding>();
-			binding.Initialize( () => _stopwatchNode.RemainingLifetimeAsZeroToOne );
+		private void HandleLoadComplete()
+		{
+			// yes this is weird, needs to be refactored for a smooth ending
+			Destroy(_binding);
+			_slider.value = 1;
+			_text.gameObject.SetActive(true);
+
+			OnEveryUpdate += CheckForClick;
+		}
+
+		private void CheckForClick(float obj)
+		{
+			if (!Input.GetMouseButtonUp(0))
+				return;
+
+			SceneManager.LoadScene("scene1");
 		}
 	}
 }
