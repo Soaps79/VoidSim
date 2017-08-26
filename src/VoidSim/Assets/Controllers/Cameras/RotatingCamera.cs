f1012979e437aa13d09f5f126f0757fa713620b9
dansopel <dansopel@gmail.com>
2017-08-26 16:48:50 -0400

    Added first version of menu logo

---
+++ src/VoidSim/Assets/Controllers/Cameras/RotatingCamera.cs
@@ -0,0 +1,21 @@
+﻿using QGame;
+using UnityEngine;
+
+namespace Assets.Controllers.Cameras
+{
+	public class RotatingCamera : QScript
+	{
+		public Camera Camera;
+		public Vector3 Rotation;
+
+		void Start()
+		{
+			OnEveryUpdate += RotateCamera;
+		}
+
+		private void RotateCamera(float obj)
+		{
+			Camera.transform.Rotate(Rotation);
+		}
+	}
+}
