407edbc3633ad4358b32b3181ed0998d677abc7c
dansopel <dansopel@gmail.com>
2018-02-26 22:09:33 -0500

    Added Occupancy tests

--- src/VoidSim/Assets/Editor/OccupancyTests.cs
+++ src/VoidSim/Assets/Editor/OccupancyTests.cs
@@ -52,19 +52,43 @@ namespace Assets.Editor
         }
 
         [Test]
+        public void PopContainer_RemoveReserve()
+        {
+            const int maxOccupancy = 1;
+            const int id = 55;
+            var person = new Person { Id = id };
+
+            var container = new PopContainer(new PopContainerParams { MaxCapacity = maxOccupancy });
+            container.AddReserved(person);
+            container.RemoveReserved(person);
+            
+            Assert.AreEqual(maxOccupancy, container.CurrentOccupancy.Count);
+
+            var occupancy = container.CurrentOccupancy.First();
+            Assert.IsFalse(occupancy.IsReserved);
+        }
+
+        [Test]
         public void PopContainer_PlaceInReserve()
         {
             const int maxOccupancy = 5;
-            const int id = 55;
-            var person = new Person {Id = id};
+            const int firstId = 55;
+            var firstPerson = new Person { Id = firstId };
+
+            const int secondId = 56;
+            var secondPerson = new Person { Id = secondId };
 
             var container = new PopContainer(new PopContainerParams {MaxCapacity = maxOccupancy});
-            container.AddReserved(person);
-            container.AddPerson(person);
-            var occupancy = container.CurrentOccupancy.FirstOrDefault(i => i.ReservedBy == id);
-            Assert.IsNotNull(occupancy);
-            Assert.IsNotNull(occupancy.OccupiedBy);
-            Assert.AreEqual(id, occupancy.OccupiedBy.Id);
+            container.AddReserved(firstPerson);
+            container.AddReserved(secondPerson);
+            container.AddPerson(firstPerson);
+
+            var occupied = container.CurrentOccupancy.Where(i => i.IsOccupied).ToList();
+            Assert.AreEqual(1, occupied.Count);
+
+            var occupancy = occupied.First();
+            Assert.AreEqual(firstPerson, occupancy.OccupiedBy);
+            Assert.AreEqual(firstId, occupancy.ReservedBy);
         }
 
         [Test]
@@ -75,7 +99,7 @@ namespace Assets.Editor
             var firstPerson = new Person {Id = firstId };
 
             const int secondId = 56;
-            var secondPerson = new Person { Id = firstId };
+            var secondPerson = new Person { Id = secondId };
 
             var container = new PopContainer(new PopContainerParams { MaxCapacity = maxOccupancy });
             container.AddPerson(firstPerson);
