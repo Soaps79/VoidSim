fe295400dbaa31eec9cbfc791a66178ba0976e14
dansopel <dansopel@gmail.com>
2018-03-05 10:33:22 -0500

    Cleared out needless pop container redraws

--- src/VoidSim/Assets/Placeables/UI/PopContainerViewModel.cs
+++ src/VoidSim/Assets/Placeables/UI/PopContainerViewModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Assets.Placeables.Nodes;
-using Assets.WorldMaterials.Population;
-using QGame;
+﻿using Assets.Placeables.Nodes;
 using UIWidgets;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Assets.Placeables.UI
 {
@@ -17,49 +11,13 @@ namespace Assets.Placeables.UI
     {
         private PopContainer _container;
         private int _lastReserveCount;
-        private readonly List<OccupantViewModel> _occupants = new List<OccupantViewModel>();
-
+        
         public void Initialize(PopContainer popContainer)
         {
             DataSource = popContainer.CurrentOccupancy.ToObservableList();
             _container = popContainer;
-            popContainer.OnUpdate += UpdateContainer;
-
+            // currently will not handle container resizing
             UpdateContainer();
-        }
-
-        private void RedrawAll()
-        {
-            ClearOccupants();
-
-            foreach (var occupancy in _container.CurrentOccupancy)
-            {
-                AddOccupantAvatar(occupancy);
-            }
-        }
-
-        void Update()
-        {
-            int i = 9;
-        }
-
-        private void ClearOccupants()
-        {
-            if (_occupants.Count == 0)
-                return;
-
-            for (int i = 0; i < _occupants.Count; i++)
-            {
-                Destroy(_occupants[i].gameObject);
-            }
-            _occupants.Clear();
-        }
-
-        private void AddOccupantAvatar(Occupancy occupancy)
-        {
-            //var avatar = Instantiate(_personPrefab, transform, false);
-            //avatar.Initialize(occupancy);
-            //_occupants.Add(avatar);
         }
 
         private void UpdateContainer()
@@ -69,24 +27,8 @@ namespace Assets.Placeables.UI
             if (rows < 1) rows = 1;
             var rect = GetComponent<RectTransform>();
             var width = Layout.GridConstraintCount * (itemWidth + Layout.Spacing.x) + Layout.GetMarginLeft() + Layout.GetMarginRight();
-            //width -= Layout.Spacing.x;
             var height = rows * (itemHeight + Layout.Spacing.y) + Layout.GetMarginTop() + Layout.GetMarginBottom();
-            //height -= Layout.Spacing.y;
             rect.sizeDelta = new Vector2(width, height);
-            
-            //if (_container.MaxCapacity != _occupants.Count)
-            //    RedrawAll();
-            //else if (_lastReserveCount != _container.Reserved)
-            //    UpdateAll();
         }
-
-        //private void UpdateAll()
-        //{
-        //    var count = _container.CurrentOccupants.Count;
-        //    for (int i = 0; i < _occupants.Count; i++)
-        //    {
-        //        _occupants[i].UpdateValues();
-        //    }
-        //}
     }
 }
